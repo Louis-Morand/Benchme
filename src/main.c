3ee29cf73d343efa49be15cb0bcac026c3cfa212
opystoglyphe <louis.morand8.5@gmail.com>
2021-10-08 12:40:32 +0200

    NEW: V1 de l'algo de test pur les valeurs voulues

--- src/main.c
+++ src/main.c
@@ -15,7 +15,33 @@ Pour les tests: incluer blibliotheque time pour mesurer le temps d'exécution de
 mettre 1 time avant 1 time apres et faire la diff des 2 pour voir temps d'execution
 TODO: fait une fonction pour mettre au format csv => plus propre ?
 */
-int main(){
+//Attention, tri à bulle limité à 10⁵ maximum
+
+
+float* crea_tab(taille_tab, graine){
+    //Fonction de création et remplissage du tableau selon les paramètres donnés.
+    float tableau[taille_tab];
+    srand(graine);
+    for(int i=0;i<taille_tab;i++){
+        tableau[i]= ((rand() % pow(10,6))+1);
+    }
+    return tableau;
+}
+
+void test_generique(){//renommer et changer pour chaque algo de tri
+    float *tableau;
+    int taille=100;
+    for (int j = 0; j < 3; j++){//on veux tester chaque algo 3 fois avec des tableaux différents
+        for(int i=0;i<6;i++){//on veux des tableau de 100 à 10⁷ valeurs, donc 6 itérations
+            tableau = crea_tab(taille, j);
+            //TODO: mettre tests relatifs à l'exécution par algo
+            // Mettre résultat dans fichier
+            taille = taille*10;
+        }
+    }
+}
+
+void main(){
     
     int taille = 10;
     float tableau[taille];
@@ -23,73 +49,14 @@ int main(){
         tableau[i]= ((rand() % 1000000)+1);
     }
 
-    float tab1[taille];
-    for(int i=0;i<=taille; i++){
-        tab1[i] = tableau[i];
-    }
-    
-    printf("\n\nINSERTION - TABLEAU AVANT:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.f ", tab1[i]);
-    }
 
-    tri_insertion(tab1, taille);
+    tri_insertion(tableau, taille);
 
-    printf("\nINSERTION - TABLEAU APRES:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab1[i]);
-    }
+    tri_selection(tableau, taille);
 
-    float tab2[taille];
-    for(int i=0;i<=taille; i++){
-        tab2[i] = tableau[i];
-    }
+    tri_tas(tableau, taille);
 
-    printf("\n\nSELECTION - TABLEAU AVANT:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.f ", tab2[i]);
-    }
-
-    tri_insertion(tab2, taille);
-
-    printf("\nSELECTION - TABLEAU APRES:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab2[i]);
-    }
-
-    float tab3[taille];
-    for(int i=0;i<=taille; i++){
-        tab3[i] = tableau[i];
-    }
-
-	printf("\n\nTAS - TABLEAU AVANT:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab3[i]);
-    }
-
-    tri_tas(tab3, taille);
-
-    printf("\nTAS - TABLEAU APRES:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab3[i]);
-    }
-
-    float tab4[taille];
-    for(int i=0;i<=taille; i++){
-        tab4[i] = tableau[i];
-    }
-
-    printf("\n\nBULLE - TABLEAU AVANT:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab4[i]);
-    }
-    
-    tri_bulle(tab4, taille);
-
-    printf("\nBULLE - TABLEAU APRES:\n");
-    for (int i = 0; i<=taille; i++){
-        printf("%.2f ", tab4[i]);
-    }
+    tri_bulle(tableau, taille);
 
 	return (0);
 }
