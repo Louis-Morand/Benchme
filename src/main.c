121c4e22a762bd3db36bba66e7251c5dc1bfaa2b
benoit-arquilliere <benoit.arquilliere@gmail.com>
2021-10-08 10:35:34 +0200

    Update main

--- src/main.c
+++ src/main.c
@@ -32,18 +32,26 @@ int main(int argc, char *argv[])
 
     printf("%c\n", " ");
 
-	printf("\nBULLE - TABLEAU AVANT:\t");
+	printf("\nTAS - TABLEAU AVANT:\t");
     for (int i = 0; i<=4; i++){
         printf("%d ", tableau2[i]);
     }
-<<<<<<< HEAD
 
-	tri_bulle(tableau2, 5);
-=======
-	tri_insertion(tableau);
 	//tri_bulle(tableau2, 5);
     tri_tas(tableau2, 5);
->>>>>>> dbef06cea0d4512407eec92e891947f4deff0ba6
+
+    printf("\nTAS - TABLEAU APRES:\t");
+    for (int i = 0; i<=4; i++){
+        printf("%d ", tableau2[i]);
+    }
+
+    	printf("\nBULLE - TABLEAU AVANT:\t");
+    for (int i = 0; i<=4; i++){
+        printf("%d ", tableau2[i]);
+    }
+    
+	//tri_bulle(tableau2, 5);
+    tri_bulle(tableau2, 5);
 
     printf("\nBULLE - TABLEAU APRES:\t");
     for (int i = 0; i<=4; i++){
