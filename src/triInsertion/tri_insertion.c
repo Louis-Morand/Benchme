f651468c1300cbea89f0495ba82e40b5fa8c786a
benoit-arquilliere <78789210+benoit-arquilliere@users.noreply.github.com>
2021-10-09 16:07:01 +0200

    Update tri_insertion.c

--- src/triInsertion/tri_insertion.c
+++ src/triInsertion/tri_insertion.c
@@ -3,7 +3,7 @@ Fonction tri_bulle
 Auteur: Louis MORAND, Benoit ARQUILLIERE
 Version : 1
 But:faire le tri d'un tableau donné en parmètre par insertion
-Soit: sélectionner une valeur, et la déplacer dans le tableau jusuq'a ce qu'elle soit à sa place
+Soit: sélectionner une valeur, et la déplacer dans le tableau jusqu'a ce qu'elle soit à sa place
 */
 #include <stdio.h>
 #include "tri_insertion.h"
@@ -15,7 +15,7 @@ void tri_insertion(float *tab, int tailletab)
 {
    for (i=1; i<tailletab; ++i) {
        val=tab[i];
-       for (j=i; j>0 && tab[j-1]>val; j--) tab[j]=tab[j-1];
+       for (j=i; j>0 && tab[j-1]>val; j--) tab[j]=tab[j-1]; //compare avec la cellule précedente, et decale jusqu'à la bonne position
        tab[j]=val;
    }
 }
